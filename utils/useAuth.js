fcf8d39fd534f1cc8e65a961b69e1cad9b43a5d7
maggie-j-liu <maggie.j.liu@gmail.com>
2021-12-10 09:13:15 -0800

    insert user data after logging in

--- utils/useAuth.js
+++ utils/useAuth.js
@@ -13,8 +13,26 @@ export const AuthProvider = ({ children }) => {
     } else {
       setAuth({ ...initialState, loading: false });
     }
-    return supabase.auth.onAuthStateChange((_event, session) => {
+    return supabase.auth.onAuthStateChange(async (_event, session) => {
       if (session) {
+        if (!session.user.user_metadata.profile_added) {
+          await supabase.from("profiles").insert(
+            {
+              id: user.id,
+              username:
+                user.user_metadata.name ||
+                user.user_metadata.full_name ||
+                user.email.split("@")[0],
+            },
+            {
+              returning: "minimal",
+            }
+          );
+          const { user } = await supabase.auth.update({
+            data: { profile_added: true },
+          });
+          session.user = user;
+        }
         setAuth({ user: session.user, session, loading: false });
       } else {
         setAuth({ ...initialState, loading: false });
