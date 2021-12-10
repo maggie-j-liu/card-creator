092bc788ed7288b24aeac2b812656d8b998e1e93
maggie-j-liu <maggie.j.liu@gmail.com>
2021-12-10 10:01:22 -0800

    list user's created cards

--- pages/cards/index.js
+++ pages/cards/index.js
@@ -1,15 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useAuth from "../../utils/useAuth";
 import { useRouter } from "next/router";
 import { supabase } from "../../utils/supabaseClient";
+import Link from "next/link";
 
 const Cards = () => {
   const { user, loading } = useAuth();
   const router = useRouter();
+  const [cards, setCards] = useState([]);
   useEffect(() => {
-    if (!loading && !user) {
+    if (loading) return;
+    if (!user) {
       router.replace("/sign-in");
+      return;
     }
+    const getCards = async () => {
+      const { data } = await supabase
+        .from("cards")
+        .select("id")
+        .eq("creator", user.id);
+      setCards(data);
+    };
+    getCards();
   }, [user, loading]);
   const createCard = async () => {
     const { data } = await supabase.from("cards").insert({
@@ -20,10 +32,18 @@ const Cards = () => {
   };
   return (
     <div>
-      create a card
+      <h1>create a card</h1>
       <button type="button" onClick={() => createCard()}>
         Create
       </button>
+      <h2>your cards</h2>
+      {cards.map((card) => (
+        <div>
+          <Link href={`/cards/${card.id}`}>
+            <a>{card.id}</a>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
