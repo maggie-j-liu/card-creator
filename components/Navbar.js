d2e8c632fdf9a33ff0ed00b3f26e0cca0863362c
Arash <47273556+arashnrim@users.noreply.github.com>
2021-12-11 11:44:49 +0800

    Change image item at Navbar to use the Next.js Image component

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { supabase } from "../utils/supabaseClient";
 import useAuth from "../utils/useAuth";
 import Avatar from "boring-avatars";
@@ -6,8 +7,8 @@ import Avatar from "boring-avatars";
 const Navbar = () => {
   const { user, loading } = useAuth();
   return (
-    <nav className="h-16 bg-red-100 px-8">
-      <div className="h-full max-w-5xl mx-auto flex items-center justify-between">
+    <nav className="h-16 px-8 bg-red-100">
+      <div className="flex items-center justify-between h-full max-w-5xl mx-auto">
         <div className="flex items-center gap-8">
           <Link href="/">
             <a>Card Creator</a>
@@ -21,10 +22,14 @@ const Navbar = () => {
             (user ? (
               <>
                 {user.user_metadata.avatar_url ? (
-                  <img
-                    className="h-8 w-8 rounded-full mr-2"
-                    src={user.user_metadata.avatar_url}
-                  />
+                  <div className="relative w-8 h-8 mr-2">
+                    <Image
+                    className="rounded-full"
+                      src={user.user_metadata.avatar_url}
+                      alt="Profile picture"
+                      layout="fill"
+                    />
+                  </div>
                 ) : (
                   <Avatar
                     variant="marble"
