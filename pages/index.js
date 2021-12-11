1166e79b8c1700a8930fdbe96f20433a5c707467
eilla1 <git@ella.cx>
2021-12-10 19:24:36 -0800

    Fix wip homepage thing

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
 import Link from "next/link";
 
 export default function Home() {
-  const cards = false;
   return (
     <main className="max-w-5xl mx-6 md:mx-14 lg:mx-32 xl:mx-auto">
-      <h1 className="leading-loose">Welcome to -!</h1>
+      <h1 className="leading-loose">Welcome to hi!</h1>
+      <h2 className="leading-loose">Recent</h2>
       {cards ? (
         <div>
-          <h2 className="leading-loose">Recent</h2>
+          <p></p>
         </div>
       ) : (
-        ""
+        <p>
+          No recent cards. Create a{" "}
+          <Link href="/create" passHref>
+            <a className="hover:underline text-red-400">new one</a>
+          </Link>
+          ?
+        </p>
       )}
     </main>
   );
